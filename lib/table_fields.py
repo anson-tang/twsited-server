6548bad2415dd5a6c356abb4ba07fd9827ecefde
anson <123@qq.com>
2016-04-17 09:03:57 +0800

    add table fields

--- lib/table_fields.py
+++ lib/table_fields.py
@@ -7,20 +7,14 @@
 # Summary: all table fields
 # about deleted: when 1-invalid 0-normal
 
-TABLE_HAD_DELETED = ('ongoing', 'building', 'adventure', 'item', 'quest', 'mail', 'wild', 'soldier')
+TABLE_HAD_DELETED = ('pvp_room', )
 TABLE_NO_AI = ('character', )
 
 
 TABLE_FIELDS = {
-    'character': (('id', 'openid', 'nickname', 'gem', 'golds', 'burg_num', 'item_capacity', \
-        'quest_over', 'vip_end_time', 'create_time'),
-        ('vip_end_time', 'create_time')),
-    'burg': (('id', 'uid', 'burg_id', 'is_current', 'name', 'food', 'wood', 'stone', 'steel', 'building_queue', \
-        'soldier_queue', 'storage_max', 'protect_max', 'vehicle_max', 'wild_max', 'soldier_max', 'food_per_hour', \
-        'wood_per_hour', 'stone_per_hour', 'steel_per_hour', 'level', 'exp', 'strength', \
-        'might', 'attack_percent', 'hp_percent', 'resource_add', 'might_add', 'rate', \
-        'status', 'total_point', 'death_time', 'last_strength_time', 'last_adventure_time', \
-        'last_resource_time', 'create_time', 'px', 'py'), 
-        ('death_time', 'last_strength_time', 'last_adventure_time', 'last_resource_time', 'create_time', )),
+    'character': (('id', 'machine_code', 'nickname', 'exp', 'coin', 'create_time'),
+        ('create_time',)),
+    'pvp_room': (('id', 'room_id', 'rank', 'scores', 'join_time', 'leave_time', 'deleted'),
+        ('join_time', 'leave_time')),
 }
 
