b91c0722e11cea27939128f67169759640c44d1a
anson <123@qq.com>
2016-04-17 13:02:39 +0800

    init database user data.

---
+++ lib/redis_constant.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*-coding: utf-8-*-
+
+# Version: 0.1
+# Author: Anson Tang <anson.tkg@gmail.com>
+# License: Copyright(c) 2015 Anson.Tang
+# Summary: 
+
+HASH_ACCOUNT_REGISTERED = ''
