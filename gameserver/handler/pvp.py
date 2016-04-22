90c4c5dd142149bd28fa8ecead27e55f150aab9d
anson <123@qq.com>
2016-04-22 10:25:06 +0800

    user random xyz

--- gameserver/handler/pvp.py
+++ gameserver/handler/pvp.py
@@ -24,14 +24,12 @@ def joinPVP(p, req):
     result['spineball'] = get_all_spineball()
 
     radius = COMMON_RADIUS * COORDINATE_ENLARGE / RADIUS_ENLARGE
-    x = random.randint(-radius, radius)
-    y = random.choice((-1, 1)) * int(math.sqrt(radius**2 - x**2))
-    z = random.choice((-1, 1)) * int(math.sqrt(radius**2 - x**2 - y**2))
+    y = random.randint(-radius, radius)
+    max_z = int(math.sqrt(radius**2 - y**2))
+    z = random.randint(-max_z, max_z)
+    x = random.choice((-1, 1)) * int(math.sqrt(radius**2 - y**2 - z**2))
 
     ballid = 1
-    #x = x * 1.0 / radius if x else 0
-    #y = y * 1.0 / radius if y else 0
-    #z = z * 1.0 / radius if z else 0
     #TODO 数据存内存 包括ballid
     result['userball'] = (ballid, x,y,z)
 
